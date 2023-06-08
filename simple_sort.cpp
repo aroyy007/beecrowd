83e684a5dc728c97adfeca18da72ffd3545be3e1
Arijit Roy <120275227+aroyy007@users.noreply.github.com>
2023-06-09 02:23:18 +0600

    Create simple_sort.cpp

---
+++ simple_sort.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    int arr[] = {a, b, c};
+    sort(arr, arr + 3);
+
+    for (int i = 0; i < 3; i++) {
+        cout << arr[i] << endl;
+    }
+
+    cout << endl;
+
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+
+    return 0;
+}
